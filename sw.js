61bba4cc2b0a07d0306002c4212e0f52fdcc11a0
Justin Catenza <justincatenza@outlook.com>
2019-05-23 11:01:03 -0500

    initalize

---
+++ sw.js
@@ -0,0 +1,45 @@
+const myCurrentCache = 'CacheV1';
+const allCaches = [
+  'Project1.html',
+  'style.css',
+  'iphone7.png',
+  'iphone6swhite.png',
+  'iphone6sblack.png',
+  'applelogo800px.jpg',
+  'apple_logo-512.png',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(myCurrentCache).then(function(cache) {
+      return cache.addAll(allCaches)
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName != myCurrentCache;
+        }).map(function(cName) {
+          return caches.delete(cName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(myCurrentCache).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
